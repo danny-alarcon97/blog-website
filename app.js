9ccee90adeabf4f7d273f5af32da95d414916ac3
daniel <danny.alarcon1997@gmail.com>
2022-11-03 20:01:53 -0400

    updated compose post that allows posts to be console logged

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 //Home Page
 app.get("/", function (req, res) {
   res.render("home", { startingContent: homeStartingContent });
+  console.log(posts);
 });
 
 //About Page
@@ -39,8 +42,14 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = req.body.postTitle;
-  console.log(post);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
